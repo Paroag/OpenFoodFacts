d66b421967b1c5a0104b91d68c4608a7d5f7dfa5
Oscar  Barbier <oscarbarbier@MacBook-Pro-de-Oscar.local>
2019-10-01 16:27:08 +0200

    InceptionV3 4 brands on GC with 90+% accuracy

---
+++ data_preparation.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import numpy as np
+import pickle
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator    
+
+def data_prep(data_dir = "../Data/Scrap_Marques/", target_dir = "./pickle/", duplication_factor = 1, max_rotation_range = 0):
+    
+    marques = ["Barilla", "Bjorg", "Gerble", "Panzani"]
+    
+    # dictionnaire d'images sous la forme :
+    # {path_image : label}
+    
+    dic_images = {}
+    for index, marque in enumerate(marques) :
+        path_images = data_dir + "Scrap_" + marque + "/Images/"
+        for file_name in os.listdir(path_images) :
+            if not file_name.startswith('.') :
+                dic_images[path_images+file_name] = index
+
+    # creation des numpy array X & Y
+    X = np.zeros((len(dic_images.keys())*duplication_factor,200,200,3))
+    Y = np.zeros((len(dic_images.keys())*duplication_factor, len(marques)))
+    
+    for index, key in enumerate(dic_images) :
+        img = load_img(key, target_size = (200,200))
+        img = img_to_array(img)
+        samples = np.expand_dims(img, 0)
+        datagen = ImageDataGenerator(rotation_range = max_rotation_range)
+        it = datagen.flow(samples, batch_size=1)
+        for k in range(duplication_factor) :
+            batch = it.next()
+            img = batch[0]
+            X[index*duplication_factor + k, :,:,:] = img/255
+            Y[index*duplication_factor + k, dic_images[key]] = 1
+            
+    # shuffle de X, Y   
+    rand = np.arange(len(dic_images.keys())*duplication_factor)
+    np.random.shuffle(rand)
+    
+    X = X[rand,:,:,:]
+    Y = Y[rand,:]
+    
+    # split en train / val (no test yet)
+    
+    split = 0.8
+    
+    X_train = X[:round(X.shape[0]*split),:,:,:]
+    Y_train = Y[:round(X.shape[0]*split),:]
+    
+    X_val = X[round(X.shape[0]*split):,:,:,:]
+    Y_val = Y[round(X.shape[0]*split):,:]
+    
+    
+    # saving data
+    
+    split_factor = 4
+    
+    X_train1 = X_train[:int(X_train.shape[0]/split_factor),:,:,:]
+    X_train2 = X_train[int(X_train.shape[0]/split_factor):int(X_train.shape[0]/split_factor*2),:,:,:]
+    X_train3 = X_train[int(X_train.shape[0]/split_factor)*2:int(X_train.shape[0]/split_factor*3),:,:,:]
+    X_train4 = X_train[int(X_train.shape[0]/split_factor)*3:int(X_train.shape[0]),:,:,:]
+    
+    pickle.dump(X_train1, open(target_dir + 'X_train1.pickle', 'wb'), protocol=2)
+    pickle.dump(X_train2, open(target_dir + 'X_train2.pickle', 'wb'), protocol=2)
+    pickle.dump(X_train3, open(target_dir + 'X_train3.pickle', 'wb'), protocol=2)
+    pickle.dump(X_train4, open(target_dir + 'X_train4.pickle', 'wb'), protocol=2)
+    pickle.dump(Y_train, open(target_dir + 'Y_train.pickle', 'wb'), protocol=2)
+    pickle.dump(X_val, open(target_dir + 'X_val.pickle', 'wb'), protocol=2)
+    pickle.dump(Y_val, open(target_dir + 'Y_val.pickle', 'wb'), protocol=2)
+    
+    return
+
+if __name__ == "__main__" :
+    
+    data_prep(duplication_factor = 3, max_rotation_range = 30)
---
+++ trainer/OpenFoodFact_inceptionv3.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#import os
+#mport time
+
+import argparse
+import pickle
+import numpy as np
+import io
+
+from tensorflow.python.lib.io import file_io
+
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from keras.callbacks import EarlyStopping
+
+"""
+Se base sur le guide :
+    https://blog.francium.tech/training-deep-learning-models-with-google-cloud-ml-engine-432ae46068d8
+"""
+
+
+def train(job_dir) :
+    
+    ### GET THE DATA
+    
+    train_path = "gs://keras-image-dataforgood/"
+    
+    print("Loading Y_val ...")
+    Y_val = pickle.load(file_io.FileIO(train_path + "Y_val.pickle", mode="rb"))
+    
+    print("Loading X_val ...")
+    X_val = pickle.load(file_io.FileIO(train_path + "X_val.pickle", mode="rb"))
+    
+    print("Loading X_train1 ...")
+    X_train1 = pickle.load(file_io.FileIO(train_path + "X_train1.pickle", mode="rb"))
+    print("Loading X_train2 ...")
+    X_train2 = pickle.load(file_io.FileIO(train_path + "X_train2.pickle", mode="rb"))
+    print("Loading X_train3 ...")
+    X_train3 = pickle.load(file_io.FileIO(train_path + "X_train3.pickle", mode="rb"))
+    print("Loading X_train4 ...")
+    X_train4 = pickle.load(file_io.FileIO(train_path + "X_train4.pickle", mode="rb"))
+
+    X_train = np.concatenate((X_train1, X_train2, X_train3, X_train4), axis=0)
+    
+    del X_train1, X_train2, X_train3, X_train4
+    
+    print("Loading Y_train ...")
+    Y_train = pickle.load(file_io.FileIO(train_path + "Y_train.pickle", mode="rb"))
+
+    
+    """
+    print("Loading X_train ...")
+    X_train = pickle.load(file_io.FileIO(train_path + "X_train.pickle", mode="rb"))
+    print("DONE")
+        print("Loading X_train ...")
+    X_train = pickle.load(io.BytesIO(file_io.read_file_to_string(train_path + "X_train.pickle", binary_mode=True)))
+    print("Loading Y_train ...")
+    Y_train = pickle.load(io.BytesIO(file_io.read_file_to_string(train_path + "Y_train.pickle", binary_mode=True)))
+    print("Loading X_val ...")
+    X_val = pickle.load(io.BytesIO(file_io.read_file_to_string(train_path + "X_val.pickle", binary_mode=True)))
+    print("Loading Y_val ...")
+    Y_val = pickle.load(io.BytesIO(file_io.read_file_to_string(train_path + "Y_val.pickle", binary_mode=True)))
+    # ref : https://stackoverflow.com/questions/44657902/how-to-load-numpy-npz-files-in-google-cloud-ml-jobs-or-from-google-cloud-storage?rq=1
+    """
+            
+   ### KERAS MODEL
+
+    base_model = InceptionV3(weights='imagenet', include_top=False)
+    
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.20)(x)
+    predictions = Dense(4, activation='softmax')(x) ## TODO change static 4
+    model = Model(inputs=base_model.input, outputs=predictions)
+    
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics = ["accuracy"])
+    
+    es = EarlyStopping(monitor='val_loss', patience=15)
+    model.fit(X_train, Y_train, epochs=100, batch_size= 32, verbose = 1, validation_data = (X_val, Y_val), callbacks = [es])  
+    
+    model.save("cloud_ml_model.h5")
+    
+    with file_io.FileIO("cloud_ml_model.h5", mode = "rb") as input_f :
+        with file_io.FileIO(job_dir + '/cloud_ml_model.h5', mode = "wb+") as output_f :
+            output_f.write(input_f.read())
+    
+if __name__ == "__main__" :  
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+          '--job-dir',
+          required=True
+        )
+
+    args = parser.parse_args()
+    arguments = args.__dict__
+    job_dir = arguments.pop('job_dir')
+
+    train(job_dir)
